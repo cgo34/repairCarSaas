e1951c02bf7866d40e57f2248ecf8c6f733797d4
Geoffroy CAPDEVILLE <gcapdeville@smag.tech>
2025-01-26 16:09:28 +0100

    chore: add ESLint configuration

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,13 +1,21 @@
 module.exports = {
     root: true,
     env: {
-      node: true
+      node: true,
     },
     extends: [
-      'plugin:vue/vue3-essential',
-      '@vue/typescript/recommended'
+      'plugin:vue/vue3-recommended',
+      '@vue/typescript/recommended',
+      'prettier', // Si vous utilisez Prettier
     ],
     parserOptions: {
-      ecmaVersion: 2020
-    }
-  }
+      ecmaVersion: 2020,
+      parser: '@typescript-eslint/parser',
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'vue/multi-word-component-names': 'off',
+    },
+  };
