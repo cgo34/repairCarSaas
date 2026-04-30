9d2487af9f3cc15aa8d2edb05af9eaf76816a67c
cgo34 <32747897+cgo34@users.noreply.github.com>
2026-04-30 21:20:43 +0200

    feat(gce): update app menu side bar and user menu (#41)

--- src/@presentation/@ui/layouts/vertical-header/ProfileDD.vue
+++ src/@presentation/@ui/layouts/vertical-header/ProfileDD.vue
@@ -25,15 +25,24 @@ const initials = (name?: string) => {
 
 <template>
   <div class="profile-dd pa-4">
-
     <!-- Avatar + infos utilisateur -->
     <div class="d-flex align-center gap-3 mb-4">
-      <v-avatar color="primary" size="44">
+      <v-avatar
+        color="primary"
+        size="44"
+      >
         <span class="text-body-1 font-weight-bold text-white">{{ initials(user?.fullName) }}</span>
       </v-avatar>
       <div class="min-w-0">
-        <div class="text-subtitle-1 font-weight-semibold text-truncate">{{ user?.fullName }}</div>
-        <v-chip size="x-small" color="primary" variant="tonal" class="mt-1">
+        <div class="text-subtitle-1 font-weight-semibold text-truncate">
+          {{ user?.fullName }}
+        </div>
+        <v-chip
+          size="x-small"
+          color="primary"
+          variant="tonal"
+          class="mt-1"
+        >
           {{ roleFr(user?.role) }}
         </v-chip>
       </div>
@@ -42,38 +51,24 @@ const initials = (name?: string) => {
     <v-divider class="mb-3" />
 
     <!-- Actions -->
-    <v-list density="compact" class="pa-0">
+    <v-list
+      density="compact"
+      class="pa-0"
+    >
       <v-list-item
         rounded="lg"
         color="primary"
         :to="'/profile'"
       >
         <template #prepend>
-          <UserIcon size="18" class="mr-3 text-medium-emphasis" />
+          <UserIcon
+            size="18"
+            class="mr-3 text-medium-emphasis"
+          />
         </template>
-        <v-list-item-title class="text-body-2">Mon profil</v-list-item-title>
-      </v-list-item>
-
-      <v-list-item
-        rounded="lg"
-        color="primary"
-        :to="'/account-settings'"
-      >
-        <template #prepend>
-          <SettingsIcon size="18" class="mr-3 text-medium-emphasis" />
-        </template>
-        <v-list-item-title class="text-body-2">Paramètres entreprise</v-list-item-title>
-      </v-list-item>
-
-      <v-list-item
-        rounded="lg"
-        color="primary"
-        :to="'/garages'"
-      >
-        <template #prepend>
-          <BuildingIcon size="18" class="mr-3 text-medium-emphasis" />
-        </template>
-        <v-list-item-title class="text-body-2">Mes garages</v-list-item-title>
+        <v-list-item-title class="text-body-2">
+          Mon profil
+        </v-list-item-title>
       </v-list-item>
 
       <v-divider class="my-2" />
@@ -85,12 +80,16 @@ const initials = (name?: string) => {
         :to="'/logout'"
       >
         <template #prepend>
-          <LogoutIcon size="18" class="mr-3" />
+          <LogoutIcon
+            size="18"
+            class="mr-3"
+          />
         </template>
-        <v-list-item-title class="text-body-2 font-weight-medium">Déconnexion</v-list-item-title>
+        <v-list-item-title class="text-body-2 font-weight-medium">
+          Déconnexion
+        </v-list-item-title>
       </v-list-item>
     </v-list>
-
   </div>
 </template>
 
