7899d7d3af7de854cade0a9b85bd336027da5def
cgo34 <32747897+cgo34@users.noreply.github.com>
2026-05-27 21:35:37 +0200

    feat(gce): add icon to text logo (#63)

--- src/@presentation/@ui/layouts/logo/LogoMain.vue
+++ src/@presentation/@ui/layouts/logo/LogoMain.vue
@@ -2,6 +2,14 @@
 import LogoDark from './LogoDark.vue';
 </script>
 <template>
-  <!-- <LogoDark /> -->
-  <h1>RepairCar</h1>
+  <div class="d-flex align-center">
+    <!-- <LogoDark /> -->
+    <v-icon
+      size="42"
+      color="blue-lighten-3"
+    >
+      mdi-car-wrench
+    </v-icon>
+    <span class="text-h2 ml-4">CarDentRepair</span>
+  </div>
 </template>
